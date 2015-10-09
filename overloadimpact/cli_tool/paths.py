b3b774e230a222e118503e1a9ee8b2be2d9b9969
Paal de Vibe <paal@menneskemaskin.no>
2015-10-09 10:33:29 +0200

    fix report template path

--- overloadimpact/cli_tool/paths.py
+++ overloadimpact/cli_tool/paths.py
@@ -37,4 +37,5 @@ SEQUENCE_PATH  = SUITE_DIR + '/suite_config/sequences/%s.yaml'
 
 RUNS_DIR = determine_run_data_dir()
 
-REPORT_TEMPLATES_DIR = os.path.abspath(__file__) + "/reports/templates"
+REPORT_TEMPLATES_DIR = os.path.dirname(os.path.abspath(__file__)) + "/reports/templates"
+print("REPORT_TEMPLATES_DIR:" + repr(REPORT_TEMPLATES_DIR))
