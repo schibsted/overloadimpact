c81b71b5c0ce8094988ae2b4e29c833504acf505
Paal de Vibe <paal@menneskemaskin.no>
2015-11-19 14:32:07 +0100

    rename suite dir to project dir

--- overloadimpact/cli_tool/project.py
+++ overloadimpact/cli_tool/project.py
@@ -5,12 +5,12 @@ def setup(name, dest_dir):
         print("Please supply a valid destination dir")
         return
 
-    defaults_base_path = os.path.dirname(os.path.realpath(__file__)) + "/../default_suite_dirs"
-    suite_dir = os.path.abspath("%s/%s_oimp_suite" % (dest_dir, name))
-    suite_run_data_dir = os.path.abspath("%s/%s_oimp_suite_run_data" % (dest_dir, name))
-    os.system("cp -rf %s/oimp_suite %s" % (defaults_base_path, suite_dir))
-    os.system("cp -rf %s/oimp_suite_run_data %s" % (defaults_base_path, suite_run_data_dir))
+    defaults_base_path = os.path.dirname(os.path.realpath(__file__)) + "/../default_project_dirs"
+    project_dir = os.path.abspath("%s/%s_oimp_project" % (dest_dir, name))
+    project_run_data_dir = os.path.abspath("%s/%s_oimp_project_run_data" % (dest_dir, name))
+    os.system("cp -rf %s/oimp_project %s" % (defaults_base_path, project_dir))
+    os.system("cp -rf %s/oimp_project_run_data %s" % (defaults_base_path, project_run_data_dir))
     print("")
-    print("Project home (%s) and project run data home (%s) successfully created." % (suite_dir, suite_run_data_dir))
+    print("Project home (%s) and project run data home (%s) successfully created." % (project_dir, project_run_data_dir))
     print("")
-    print("Add OIMP_PROJECT_HOME=%s and OIMP_PROJECT_RUN_DATA_HOME=%s to your environment variables." % (suite_dir, suite_run_data_dir))
+    print("Add OIMP_PROJECT_HOME=%s and OIMP_PROJECT_RUN_DATA_HOME=%s to your environment variables." % (project_dir, project_run_data_dir))
