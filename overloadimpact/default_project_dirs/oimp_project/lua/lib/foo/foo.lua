2486a34b99ffa97ea4d7b71082075ce72a1661e2
Paal de Vibe <paal@menneskemaskin.no>
2015-11-19 15:57:49 +0100

    improve code structure

--- overloadimpact/default_project_dirs/oimp_project/lua/lib/foo/foo.lua
+++ overloadimpact/default_project_dirs/oimp_project/lua/lib/foo/foo.lua
@@ -4,11 +4,13 @@ foo.some_var = "bar"
 function foo.some_request(foo_param)
   local page = "foo_index"
   local users_ds  = datastore.open('users-DS_VERSION') -- get a versioned users DS
-  user = users_ds:get_random()
-  return oimp.request(page, {
+  local user = users_ds:get_random()
+  local url = "http://www.examplefoo.com/index.html?user_email=" .. url.escape(user[1]) .. "&foo=" .. url.escape(foo_param)
+  return oimp.request(page,
+                      {
                         'GET',
-                        "http://www.examplefoo.com/index.html?user_email=" .. url.escape(user[1]) .. "&foo=" .. url.escape(foo_param)
-  },
+                        url,
+                      },
                       true -- is_core_action = true, signals that this the core action of this scenario
   )
 end
